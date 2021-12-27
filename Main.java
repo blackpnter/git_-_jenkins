50ee8e8000d1a1cadf69492f69f83005216f3632
blackpnter <yaakovv5234@gmail.com>
2021-12-27 20:32:00 +0200

    simple code in java that select rendom number beetin 1-37

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int lotNumbers = (int)Math.round(Math.random() * 37);
+       System.out.println(lotNumbers);
+        /*System.out.println("Please enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine().trim();
+        System.out.println("Please enter your wish:");
+        int money = scanner.nextInt();
+        System.out.println("Your name is:  " + name);
+        System.out.println( "Your wish money is:" + money);*/
+
+
+    }
+}
